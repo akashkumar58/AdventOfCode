e6c741f8cd211704c1cd88c24955165479d331f1
Akash Kumar <kumar.akash58@outlook.com>
2022-12-05 07:49:50 +0530

    day 5 challenge

---
+++ 2022/Day5/topOfStacks.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main() {
+    /** Intial stack configuration is given as follows:
+    [H]         [D]     [P]        
+[W] [B]         [C] [Z] [D]        
+[T] [J]     [T] [J] [D] [J]        
+[H] [Z]     [H] [H] [W] [S]     [M]
+[P] [F] [R] [P] [Z] [F] [W]     [F]
+[J] [V] [T] [N] [F] [G] [Z] [S] [S]
+[C] [R] [P] [S] [V] [M] [V] [D] [Z]
+[F] [G] [H] [Z] [N] [P] [M] [N] [D]
+ 1   2   3   4   5   6   7   8   9
+     */
+    vector<string> arrays(9);
+    arrays[0] = "FCJPHTW";
+    arrays[1] = "GRVFZJBH";
+    arrays[2] = "HPTR";
+    arrays[3] = "ZSNPHT";
+    arrays[4] = "NVFZHJCD";
+    arrays[5] = "PMGFWDZ";
+    arrays[6] = "MVZWSJDP";
+    arrays[7] = "NDS";
+    arrays[8] = "DZSFM";
+
+    vector<stack<char>> stacks(9);
+    for (int i = 0; i < arrays.size(); i++) {
+        for (int j = 0; j < arrays[i].size(); j++) {
+            stacks[i].push(arrays[i][j]);
+        }
+    }
+    // read the moves till EOF
+    string input;
+    while (getline(cin, input)) {
+        int quantity = 0, source = 0, target = 0;
+        int i = 5;
+        // read the quantity which is in between move and from. Starting index will always be 5.
+        while (input[i] != ' ') {
+            quantity = (quantity * 10) + (input[i++] - '0');
+        }
+        // index is pointing to space before 'from ', so we move it by 5 to point to source
+        i += 6;
+        while (input[i] != ' ') {
+            source = (source * 10) + (input[i++] - '0');
+        }
+        // index is pointing to space before 'to ', so we move it by 3 to point to target
+        i += 4;
+        while (i < input.size()) {
+            target = (target * 10) + (input[i++] - '0');
+        }
+        // pop quantity items from source stack to target stack one by one.
+        for(int j = 0; j < quantity; j++) {
+            char ch = stacks[source - 1].top();
+            stacks[source - 1].pop();
+            stacks[target - 1].push(ch);
+        }
+    }
+    // answer will be top of all stacks from left to right.
+    string ans = "";
+    for(int i = 0; i < stacks.size(); i++) {
+        ans += stacks[i].top();
+    }
+    cout << ans << endl;
+    return 0;
+}
